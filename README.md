1d9afcc03768e58192fac04c431cf9f9726f7e4c
VadimBichikhanov <bishikhan82@gmail.com>
2024-12-16 16:57:44 +0800

    Revert "first commit"
    This reverts commit 9d9ac2ac710b77cc182dceb5253ae0a9c0f16c7b.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # module_19_2
 # model_19_3
-# module_19_4
--- task1/admin.py
+++ task1/admin.py
@@ -1,36 +1,5 @@
 from django.contrib import admin
 from .models import Buyer, Game
 
-# Настройка админ-класса для модели Game
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):
-    list_filter = ('size', 'cost')  # Фильтрация по полям size и cost
-    list_display = ('title', 'cost', 'size')  # Отображение полей title, cost и size
-    search_fields = ('title',)  # Поиск по полю title
-    list_per_page = 20  # Ограничение количества записей до 20
-
-# Настройка админ-класса для модели Buyer
-@admin.register(Buyer)
-class BuyerAdmin(admin.ModelAdmin):
-    list_filter = ('balance', 'age')  # Фильтрация по полям balance и age
-    list_display = ('name', 'balance', 'age')  # Отображение полей name, balance и age
-    search_fields = ('name',)  # Поиск по полю name
-    list_per_page = 30  # Ограничение количества записей до 30
-    readonly_fields = ('balance',)
-    
-fieldsets = (
-    (None, {
-        "fields": (
-            "name",
-            "balance",
-            "age",
-        ),
-    }),
-    ("Дополнительная информация", {
-        "classes": ("collapse",),
-        "fields": (
-            "registration_date",
-        ),
-    }),
-)
-readonly_fields = ("registration_date",)
+admin.site.register(Buyer)
+admin.site.register(Game)
